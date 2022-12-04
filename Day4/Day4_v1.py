5ef90e713fa48852fd771b38a468e94b22f39dc5
Norman <rickey00x@gmail.com>
2022-12-04 06:34:32 +0100

    Day4 Solutions

---
+++ Day4/Day4_v1.py
@@ -0,0 +1,20 @@
+with open('Day4\Input.txt') as f:
+    sum = 0
+    for line in f:
+        line =line.replace("\n", "")
+        firstelf, secondelf = line.split(',')
+        firstelf_start, firstelf_end = firstelf.split('-')
+        secondelf_start, secondelf_end = secondelf.split('-')
+        firstelf_start = int(firstelf_start)
+        firstelf_end = int(firstelf_end)
+        secondelf_start = int(secondelf_start)
+        secondelf_end = int(secondelf_end)
+
+        print(firstelf_start, firstelf_end, secondelf_start, secondelf_end)
+
+        if(firstelf_start >= secondelf_start and firstelf_end <= secondelf_end):
+                sum += 1
+        elif(secondelf_start >= firstelf_start and secondelf_end <= firstelf_end):
+                sum += 1
+        
+    print (sum)
