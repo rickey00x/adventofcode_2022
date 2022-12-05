e86a23a4041e7ef463e5732780a2142c11ff8688
Norman <rickey00x@gmail.com>
2022-12-05 11:06:46 +0100

    Day5 solution part 1

---
+++ Day5/Day5_v1.py
@@ -0,0 +1,37 @@
+stack = [[]]
+
+def move(command):
+    idx = command[1]-1
+    new = command[2]-1
+    for i in range(command[0],0,-1):
+        if stack[idx]:
+            a = stack[idx][-1]
+            stack[idx].pop()
+            stack[new].append(a) 
+
+
+                  
+    
+with open('Day5\Input.txt') as f:
+    first, second = f.read().split('\n\n')
+
+
+
+
+
+# First part
+for lines in reversed(first.splitlines()):
+    for i in range(0, len(lines), 4):
+        if(lines[i+1].isnumeric()):
+            stack.append([])
+        elif lines[i+1] != ' ':
+            stack[int(i/4)].append(lines[i+1])
+
+# Second part
+for lines in second.splitlines():
+    command = [int(s) for s in lines.split() if s.isdigit()]
+    move(command)
+
+for i in range(0, len(stack)):
+    print(stack[i][-1])
+print(stack)
